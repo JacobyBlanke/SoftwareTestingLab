c312136c9829e0196a087a45c59c662006e0d670
Xohvan <jacoby.blanke@protonmail.com>
2020-10-02 10:05:20 -0400

    reorganize imports

--- src/com/company/Pair.java
+++ src/com/company/Pair.java
@@ -7,8 +7,8 @@ package com.company;
  */
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Random;
 public class Pair {
